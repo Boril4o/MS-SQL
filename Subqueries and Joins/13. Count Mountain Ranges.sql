179a709b3be5a64841f804d143a63adb1517d0f9
Boris <borisgeorgievdg@gmail.com>
2022-06-18 02:24:43 +0300

    Exercises from Joins and Built-in Functions

---
+++ Subqueries and Joins/13. Count Mountain Ranges.sql
@@ -0,0 +1,6 @@
+SELECT 
+CountryCode,
+COUNT(MountainId) AS MountainRanges
+FROM MountainsCountries
+WHERE [CountryCode] IN ('US', 'RU', 'BG')
+GROUP BY CountryCode
---
+++ Subqueries and Joins/3. Sales Employee.sql
@@ -0,0 +1,9 @@
+SELECT TOP(50)
+e.EmployeeID,
+e.FirstName,
+e.LastName,
+d.Name AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+WHERE e.DepartmentID = 3
+ORDER BY e.EmployeeID ASC
